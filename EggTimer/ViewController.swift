b0c91c619284769823e85d7cee95d5457319d60a
Saurabh Mirajkar <mirajkar46@gmail.com>
2021-07-05 21:51:42 +0530

    Challenge #1

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        switch sender.currentTitle {
+        
+        case "Soft": print(softTime); break
+        case "Medium": print(mediumTime); break
+        case "Hard": print(hardTime); break
+        default: break
+        
+        }
+        
+    }
+    
+    
+    
 }
